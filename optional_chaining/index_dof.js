6cbbcf00555ac0a5608e9a26a15281132de82e42
Alex Oliveira <alexoliveira7x@gmail.com>
2023-09-27 11:14:33 +0100

    chore: optional

--- optional_chaining/index_dof.js
+++ optional_chaining/index_dof.js
@@ -1,4 +1,4 @@
-var _a, _b, _c;
+var _a;
 var adventurer = {
     name: 'Alice',
     cat: {
@@ -6,4 +6,3 @@ var adventurer = {
     },
 };
 var dogName = ((_a = adventurer.dog) === null || _a === void 0 ? void 0 : _a.name) || 'default';
-var wolfName = (_c = (_b = adventurer.wolf) === null || _b === void 0 ? void 0 : _b.name) !== null && _c !== void 0 ? _c : 'default';
---
+++ optional_chaining/index_wolf.js
@@ -0,0 +1,8 @@
+var _a, _b;
+var adventurer = {
+    name: 'Alice',
+    cat: {
+        name: 'Dinah',
+    },
+};
+var wolfName = (_b = (_a = adventurer.wolf) === null || _a === void 0 ? void 0 : _a.name) !== null && _b !== void 0 ? _b : 'default';
