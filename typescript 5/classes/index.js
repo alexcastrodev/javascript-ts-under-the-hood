2c855e3e6f8b1e2afdd3ada34008c5464ea58cb0
alexcastrodev <alexoliveira7x@gmail.com>
2026-07-22 15:08:54 +0100

    move ts5

---
+++ typescript 5/classes/index.js
@@ -0,0 +1,22 @@
+class Calculator {
+    name;
+    constructor(name) {
+        this.name = name;
+    }
+    add(a, b) {
+        return a + b;
+    }
+    subtract(a, b) {
+        return a - b;
+    }
+    multiply(a, b) {
+        return a * b;
+    }
+    divide(a, b) {
+        if (b === 0) {
+            throw new Error("Cannot divide by zero.");
+        }
+        return a / b;
+    }
+}
+export default Calculator;
